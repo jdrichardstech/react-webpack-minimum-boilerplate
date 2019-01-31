a0e7e43fe15e05dab238d54c1e8759af64f8e7fb
jdrichardstech <jdrichardstech@gmail.com>
2019-01-30 19:50:43 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const Title = () => (
-<<<<<<< HEAD
-  <div style={{ color: "red" }}>"My Minimal React Webpack Babel Setup";</div>
-=======
-  <div style={{ color: "red" }}>
-    "My Minimal React Webpack Babel Setup";
-    <p>Image below checks to make sure npm image-loader & webpack is working</p>
-    <img
-      style={{ width: 150 }}
-      src={require("../dist/img/react.jpg")}
-      alt="bank"
-    />
-  </div>
->>>>>>> d8896b30581b2006074fbba11227169e7343acd0
+  <div style={{ color: 'red' }}>"My Minimal React Webpack Babel Setup";</div>
 );
 
-ReactDOM.render(<Title />, document.getElementById("app"));
+ReactDOM.render(<Title />, document.getElementById('app'));
 
 module.hot.accept();
